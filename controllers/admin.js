f8410101bcef04ec57368fda9f034f6a163ee978
Kuzey Özyurt <ozyurtkuzey@gmail.com>
2024-05-19 11:09:12 +0300

    Menü sistemi yapıldı

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,6 @@
 const Restaurant = require('../models/restaurants');
+const MenuItem = require('../models/menu');
+
 
 exports.getRestaurant = (req, res, next) => {
     const restaurants = Restaurant.getAll();
@@ -23,11 +25,10 @@ exports.postAddRestaurant = (req, res, next) => {
     restaurant.name = req.body.name;
     restaurant.lat = req.body.lat;
     restaurant.long = req.body.long;
-    restaurant.icon = req.body.icon;
     restaurant.menu = req.body.menu;
 
     restaurant.saveRestaurant();
-    res.redirect('/');
+    res.redirect('/admin/restaurants');
 }
 
 exports.getEditRestaurant = (req, res, next) => {
@@ -50,7 +51,6 @@ exports.postEditRestaurant = (req, res, next) => {
     restaurant.name = req.body.name;
     restaurant.lat = req.body.lat;
     restaurant.long = req.body.long;
-    restaurant.icon = req.body.icon;
     restaurant.menu = req.body.menu;
 
     Restaurant.Update(restaurant);
@@ -60,4 +60,62 @@ exports.postEditRestaurant = (req, res, next) => {
 exports.postDeleteRestaurant = (req, res, next) => {
     Restaurant.DeleteById(req.body.restaurantid);
     res.redirect('/admin/restaurants?action=delete');
+}
+
+exports.getMenuItems = (req, res, next) => {
+    const menuItems = MenuItem.getAll();
+    res.render('admin/menus', {
+        menuItems: menuItems,
+        path: '/admin/menus',
+        action: req.query.action,
+        title: 'Kolaylokma - Menüler'
+    });
+}
+
+exports.getAddMenuItems = (req, res, next) => {
+    res.render('admin/add-menu', {
+        path: '/admin/add-menu',
+        title: 'Kolaylokma - Menü Ekle'
+    });
+}
+
+exports.postAddMenuItems = (req, res, next) => {
+    const menuItem = new MenuItem();
+
+    menuItem.restaurant_id = req.body.restaurant_id;
+    menuItem.item_name = req.body.item_name;
+    menuItem.item_price = req.body.item_price;
+
+    menuItem.saveMenuItems();
+    res.redirect('/admin/menus');
+}
+
+exports.getEditMenuItems = (req, res, next) => {
+
+    const menuItem = MenuItem.getById(req.params.menuItemsid);
+
+    res.render('admin/edit-menu', {
+        path: '/admin/menus',
+        menuItem: menuItem,
+        title: 'Kolaylokma - Menü Düzenle'
+    });
+}
+
+exports.postEditMenuItems = (req, res, next) => {
+
+    console.log(req.body)
+    
+    const menuItem = MenuItem.getById(req.body.id);
+
+    menuItem.restaurant_id = req.body.restaurant_id;
+    menuItem.item_name = req.body.item_name;
+    menuItem.item_price = req.body.item_price;
+
+    MenuItem.Update(menuItem);
+    res.redirect('/admin/menus?action=edit');
+}
+
+exports.postDeleteMenuItems = (req, res, next) => {
+    MenuItem.DeleteById(req.body.menuItemsid);
+    res.redirect('/admin/menus?action=delete');
 }
