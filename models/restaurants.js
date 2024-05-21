8fb870f811b6f86944b9dd4995900dec3c37b5a0
Kuzey <42831791+Bullitt08@users.noreply.github.com>
2024-05-21 11:38:29 +0300

    Menü sistemi yapıldı (#8)

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,17 +1,16 @@
 const restaurants = [
-    {id:"11111", name:'Restoran 1', lat:41.4507,long:31.7627, icon:"https://cdn-icons-png.flaticon.com/512/4287/4287725.png", menu:'1' },
-    {id:"11112", name:'Restoran 2', lat:41.4517,long:31.7627, icon:"https://cdn-icons-png.flaticon.com/512/4287/4287725.png", menu:'2' },
-    {id:"11113", name:'Restoran 3', lat:41.4527,long:31.7627, icon:"https://cdn-icons-png.flaticon.com/512/4287/4287725.png", menu:'3' },
-    {id:"11114", name:'Restoran 4', lat:41.4517,long:31.7637, icon:"https://cdn-icons-png.flaticon.com/512/4287/4287725.png", menu:'4' }
+    {id:"11111", name:'Restoran 1', lat:41.4507,long:31.7627, menu:'1' },
+    {id:"11112", name:'Restoran 2', lat:41.4517,long:31.7627, menu:'2' },
+    {id:"11113", name:'Restoran 3', lat:41.4527,long:31.7627, menu:'3' },
+    {id:"11114", name:'Restoran 4', lat:41.4517,long:31.7637, menu:'4' }
 ];
 
 module.exports = class Restaurants {
-    constructor(id, name, lat, long, icon, menu) {
+    constructor(id, name, lat, long, menu) {
         this.id = (Math.floor(Math.random() * 99999) + 1).toString();;
         this.name = name;
         this.lat = lat;
         this.long = long;
-        this.icon = icon;
         this.menu = menu;
     }
 
@@ -33,7 +32,6 @@ module.exports = class Restaurants {
         restaurants[index].name = restaurant.name;
         restaurants[index].lat = restaurant.lat;
         restaurants[index].long = restaurant.long
-        restaurants[index].icon = restaurant.icon;
         restaurants[index].menu = restaurant.menu;
     }
 
